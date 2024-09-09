f5af51200c0722aaec626cc9db7b9d12f53ec0a4
Anitha Natarajan <51791012+anithapriyanatarajan@users.noreply.github.com>
2024-09-09 16:58:30 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,25 @@
+// main.go
+// test if the build trigger works
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!.You are TEDDYSOME!!", name)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", handleHello).Methods("GET")
+	fmt.Println("Server is listening on port 8084...")
+	http.ListenAndServe(":8084", r)
+}
